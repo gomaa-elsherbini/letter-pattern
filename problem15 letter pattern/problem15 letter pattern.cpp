2829772da3acf54dadc65b8456ecd80f8d1fb5b1
gomaa <gogochemist35@gmail.com>
2023-09-13 22:58:47 +0300

    change code

--- problem15 letter pattern/problem15 letter pattern.cpp
+++ problem15 letter pattern/problem15 letter pattern.cpp
@@ -6,29 +6,63 @@ using namespace std;
 
 int readNumber(string msg)
 {
-    int num;
-    cout << msg << endl;
-    cin >> num;
-    return num;
+	int num;
+	do
+	{
+		cout << msg << endl;
+		cin >> num;
+	} while (num < 1 || num >26);
+
+	return num;
 }
 
-void printInvertedPattern(int num)
+char getLetter(int num)
 {
-    string str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	return char(num + 64);
+}
 
-    for (int x = 0; x <= num - 1; x++)
-    {
-        for (int i = 0; i <= x; i++)
-        {
-            cout << str[x];
-        }
-        cout << "\n";
-    }
+void printLetterPattern(int num)
+{
+	for (int i = 1; i <= num; i++)
+	{
+		for (int x = 1; x <= i; x++)
+		{
+			cout << getLetter(i);
+		}
+		cout << endl;
+	}
 }
 
 int main()
 {
-    printInvertedPattern(readNumber("Please enter number?"));
-
-    return main();
+	printLetterPattern(readNumber("enter number?"));
 }
+
+//int readNumber(string msg)
+//{
+//    int num;
+//    cout << msg << endl;
+//    cin >> num;
+//    return num;
+//}
+//
+//void printInvertedPattern(int num)
+//{
+//    string str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//
+//    for (int x = 0; x <= num - 1; x++)
+//    {
+//        for (int i = 0; i <= x; i++)
+//        {
+//            cout << str[x];
+//        }
+//        cout << "\n";
+//    }
+//}
+//
+//int main()
+//{
+//    printInvertedPattern(readNumber("Please enter number?"));
+//
+//    return main();
+//}
